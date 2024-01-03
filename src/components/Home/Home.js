ce46bae6c5875b2d13e34c9188dde6c59dd31a87
dhanushxecutables <dhanush.xecutables@gmail.com>
2024-01-03 15:53:48 +0530

    images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> DHANUSH BANDI 💜</strong>
+                <strong className="main-name"> DHANUSH BANDI 👾</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
